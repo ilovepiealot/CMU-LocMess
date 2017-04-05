c046161494ae0a5c5ded4bd9ac9436bc563916a5
DizzyTurtle <DizzyTurtle>
2017-04-05 16:43:25 +0100

    background_menu inicial

--- app/src/main/java/grupo19/locmess19/MainActivity.java
+++ app/src/main/java/grupo19/locmess19/MainActivity.java
@@ -1,13 +1,22 @@
 package grupo19.locmess19;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sign_up_click(View v){
+       startActivity(new Intent(MainActivity.this, signUp.class));
+
     }
 }
---
+++ app/src/main/java/grupo19/locmess19/signUp.java
@@ -0,0 +1,18 @@
+package grupo19.locmess19;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by super on 05/04/2017.
+ */
+
+public class signUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
